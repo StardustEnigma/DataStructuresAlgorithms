5f035adbe0ccfa1b56cccb7a3bd3c4d9b16b8545
Atharva Mandle <atharvamandle19@gmail.com>
2025-12-31 20:21:35 +0530

    M2O

---
+++ src/MajorityElement2Optimal.java
@@ -0,0 +1,30 @@
+public class MajorityElement2Optimal {
+    public static void main(String[] args) {
+        int[] arr={1,1,1,1,3,2,2,2};
+        int cnt1=0;
+        int cnt2=0;
+        int elem1=0;
+        int elem2=0;
+
+        for (int i=0;i< arr.length;i++){
+            if (cnt1==0 && arr[i] !=elem2){
+                cnt1=1;
+                elem1=arr[i];
+            } else if (cnt2==0 && arr[i] != elem1) {
+                cnt2=1;
+                elem2=arr[i];
+            } else if (elem1==arr[i]) {
+                cnt1++;
+            } else if (elem2==arr[i]) {
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+
+        }
+        System.out.println(elem1);
+        System.out.println(elem2);
+    }
+}
