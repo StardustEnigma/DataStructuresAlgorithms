695b1504b4b963111c970c8d30e9d3f2398e75dc
Atharva Mandle <atharvamandle19@gmail.com>
2026-01-14 23:23:59 +0530

    spiral

---
+++ src/MergeOverlappingInterval.java
@@ -0,0 +1,12 @@
+public class MergeOverlappingInterval {
+    public static void main(String[] args) {
+        int[][] arr={
+                {1,3},
+                {2,6},
+                {8,10},
+                {15,18}
+        };
+
+        
+    }
+}
---
+++ src/SpiralMatrix.java
@@ -0,0 +1,42 @@
+public class SpiralMatrix {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int top = 0;
+        int bottom = arr.length - 1;
+        int left = 0;
+        int right = arr[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int j = left; j <= right; j++) {
+                System.out.print(arr[top][j] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(arr[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    System.out.print(arr[bottom][j] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(arr[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+}
